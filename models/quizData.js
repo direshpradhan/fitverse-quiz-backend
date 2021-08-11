a4bb369db9180e545e85fcf465891529902aac57
Diresh Kumar <41542536+diresh18@users.noreply.github.com>
2021-08-11 15:48:10 +0530

    added quiz data

---
+++ models/quizData.js
@@ -0,0 +1,248 @@
+const quizData = [
+  {
+    quizName: "Marvel Cinematic Universe(MCU)",
+    playTime: "5 minutes",
+    totalPoints: 25,
+    totalQuestions: 5,
+    description: "Are you a MCU Fan? Test your skills by taking this quiz!!",
+    imageUrl:
+      "https://rukminim1.flixcart.com/image/416/416/kavefm80/poster/w/x/z/medium-the-marvel-avengers-wall-poster-for-room-with-gloss-original-imafsckr6vt3wjjv.jpeg?q=70",
+    questions: [
+      {
+        question:
+          "What year was the first Iron Man movie released, kicking off the Marvel Cinematic Universe?",
+        points: 5,
+        options: [
+          {
+            text: "2007",
+            isRight: "False",
+          },
+          {
+            text: "2008",
+            isRight: "True",
+          },
+          {
+            text: "2009",
+            isRight: "False",
+          },
+          {
+            text: "2010",
+            isRight: "False",
+          },
+        ],
+      },
+      {
+        question: "What war did Captain America fight in?",
+        points: 5,
+        options: [
+          {
+            text: "World War 1",
+            isRight: "False",
+          },
+          {
+            text: "Vietnam",
+            isRight: "False",
+          },
+          {
+            text: "World War 2",
+            isRight: "False",
+          },
+          {
+            text: "Gulf War",
+            isRight: "False",
+          },
+        ],
+      },
+      {
+        question: "Which alien race invaded New York in The Avengers?",
+        points: 5,
+        options: [
+          {
+            text: "Chitauri",
+            isRight: "True",
+          },
+          {
+            text: "Kree",
+            isRight: "False",
+          },
+          {
+            text: "Skrulls",
+            isRight: "False",
+          },
+          {
+            text: "Shi'ar",
+            isRight: "False",
+          },
+        ],
+      },
+      {
+        question: "Who was the first major character to die?",
+        points: 5,
+        options: [
+          {
+            text: "Black Widow",
+            isRight: "False",
+          },
+          {
+            text: "Nebula",
+            isRight: "False",
+          },
+          {
+            text: "Tony Stark",
+            isRight: "False",
+          },
+          {
+            text: "Heimdall",
+            isRight: "True",
+          },
+        ],
+      },
+      {
+        question: "Who made Captain America’s shield?",
+        points: 5,
+        options: [
+          {
+            text: "Nick Fury",
+            isRight: "False",
+          },
+          {
+            text: "Howard Stark",
+            isRight: "True",
+          },
+          {
+            text: "Tony Stark",
+            isRight: "False",
+          },
+          {
+            text: "Bruce Banner",
+            isRight: "False",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    quizName: "Fitness",
+    playTime: "5 minutes",
+    totalPoints: 25,
+    totalQuestions: 5,
+    description:
+      "Are you a fitness enthusiast? Check it out by trying this quiz!!",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZJ9_z_ym29_b8cgrHYinpeZN0Vtq0rQUgjQ&usqp=CAU",
+    questions: [
+      {
+        question:
+          "Which of the following health benefits would result from regular walking?",
+        points: 5,
+        options: [
+          {
+            text: "Increased blood pressure",
+            isRight: "False",
+          },
+          {
+            text: "Increased resting heart rate",
+            isRight: "False",
+          },
+          {
+            text: "Decreased risk of CHD",
+            isRight: "True",
+          },
+          {
+            text: "Decreased life expectancy",
+            isRight: "False",
+          },
+        ],
+      },
+      {
+        question: "What is the strongest muscle in the human body?",
+        points: 5,
+        options: [
+          {
+            text: "Heart",
+            isRight: "True",
+          },
+          {
+            text: "Calve Muscle",
+            isRight: "False",
+          },
+          {
+            text: "Bicep Muscle",
+            isRight: "False",
+          },
+          {
+            text: "Gluteus Maximus",
+            isRight: "False",
+          },
+        ],
+      },
+      {
+        question: "At what rate does muscle turn to fat without exercise?",
+        points: 5,
+        options: [
+          {
+            text: "One Ounce Per Week",
+            isRight: "False",
+          },
+          {
+            text: "One Pound Per Year",
+            isRight: "False",
+          },
+          {
+            text: "One Pound Per Week",
+            isRight: "False",
+          },
+          {
+            text: "Muscle Doesn't Turn To Fat",
+            isRight: "True",
+          },
+        ],
+      },
+      {
+        question: "What is the definition of muscular strength?",
+        points: 5,
+        options: [
+          {
+            text: "The ability of muscles to exert minimum force in one contraction",
+            isRight: "False",
+          },
+          {
+            text: "The ability of muscles to exert maximum force in one contraction",
+            isRight: "True",
+          },
+          {
+            text: "The ability of muscles to repeatedly exert maximum force",
+            isRight: "False",
+          },
+          {
+            text: "The ability of muscles to repeatedly exert minimum force",
+            isRight: "False",
+          },
+        ],
+      },
+      {
+        question:
+          "Which of the following would be classified in the aerobic fitness zone?",
+        points: 5,
+        options: [
+          {
+            text: "65% max heart rate",
+            isRight: "False",
+          },
+          {
+            text: "75% max heart rate",
+            isRight: "True",
+          },
+          {
+            text: "85% max heart rate",
+            isRight: "False",
+          },
+          {
+            text: "95% max heart rate",
+            isRight: "False",
+          },
+        ],
+      },
+    ],
+  },
+];
