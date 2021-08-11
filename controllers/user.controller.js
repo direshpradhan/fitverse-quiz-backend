1771525345343f945c10e1d22b255d0799cbcbe7
Diresh Kumar <41542536+diresh18@users.noreply.github.com>
2021-08-11 17:35:45 +0530

    added middleware to authenticate user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
-const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/user.model");
 
 const getUserData = async (req, res) => {
   try {
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { initializeDBConnection } = require("./db/db.connect");
 const { addQuizzesToDatabase } = require("./models/quiz.model");
 const quizRouter = require("./routes/quiz.router");
 const userRouter = require("./routes/user.router");
+const { authenticateToken } = require("./authenticateToken");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,12 +14,13 @@ app.use(cors());
 initializeDBConnection();
 //addQuizzesToDatabase();
 
-app.use("/quiz", quizRouter);
-app.use("/user", userRouter);
-
 app.get("/", (req, res) => {
   res.send("Hello Express app!");
 });
+
+app.use("/user", userRouter);
+app.use(authenticateToken);
+app.use("/quiz", quizRouter);
 
 app.listen(3000, () => {
   console.log("server started");
